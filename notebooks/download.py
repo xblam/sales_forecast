e05ec9b1366a34e72cfe037b290b5f34ec0eb992
xblam <benlam05032004@gmail.com>
2025-08-04 01:12:40 +0700

    made visuals of test and train set

--- notebooks/download.py
+++ notebooks/download.py
@@ -2,4 +2,4 @@ import kaggle
 from kaggle.api.kaggle_api_extended import KaggleApi
 kaggle.api.authenticate()
 
-kaggle.api.dataset_download_files('anandshaw2001/top-spotify-songs-in-countries', path='./input', unzip=True)
+kaggle.api.dataset_download_files("robikscube/hourly-energy-consumption", path='./input', unzip=True)
